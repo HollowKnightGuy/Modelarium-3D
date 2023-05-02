c6145229646e5877ed824e70b0c615c1deb30631
Pablo Cid <propableras@gmail.com>
2023-05-02 13:50:13 +0200

    forms and styles

---
+++ public/main.js
@@ -0,0 +1,4 @@
+const selectFileBtn = document.getElementsByClassName("select-file-btn")[0];
+const selectFileInput = document.getElementById("file");
+
+selectFileBtn.addEventListener("click", getFile)
